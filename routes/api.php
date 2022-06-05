cbee24bc37c8f91689ee056222181a99228fc4ba
Mercuuury <kovshiksa@gmail.com>
2022-06-05 13:40:55 +0300

    api v1 by @DeaphyGod

--- routes/api.php
+++ routes/api.php
@@ -4,19 +4,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApplicationController;
 use App\Http\Controllers\ApplicationStatusController;
+use App\Http\Controllers\AudienceController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EmployerController;
 use App\Http\Controllers\EmployerStatusController;
+use App\Http\Controllers\EventController;
 use App\Http\Controllers\FacultyController;
+use App\Http\Controllers\NewsController;
 use App\Http\Controllers\OrganizationController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\SocialController;
 use App\Http\Controllers\SpecialityController;
 use App\Http\Controllers\StageController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VacancyController;
-use App\Http\Controllers\EventController;
-use App\Http\Controllers\NewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -38,6 +40,39 @@ use App\Http\Controllers\NewsController;
 // Auth
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+// Audience
+Route::get('/audience', [AudienceController::class, 'index']);
+Route::get('/audience/{id}', [AudienceController::class, 'show']);
+// Events
+Route::get('/events', [EventController::class, 'index']);
+Route::get('/events/{id}', [EventController::class, 'show']);
+// News
+Route::get('/news', [NewsController::class, 'index']);
+Route::get('/news/{id}', [NewsController::class, 'show']);
+// Faculties
+Route::get('/faculties', [FacultyController::class, 'index']);
+Route::get('/faculties/{id}', [FacultyController::class, 'show']);
+// Vacancies
+Route::get('/vacancies', [VacancyController::class, 'index']);
+Route::get('/vacancies/{id}', [VacancyController::class, 'show']);
+// Vacancy types
+Route::get('/vacancy-types', [VacancyTypeController::class, 'index']);
+Route::get('/vacancy-types/{id}', [IndustryController::class, 'show']);
+// Vacancy function
+Route::get('/vacancy-functions', [VacancyFunctionController::class, 'index']);
+Route::get('/vacancy-functions/{id}', [VacancyFunctionController::class, 'show']);
+// Employers
+Route::get('/employers', [EmployerController::class, 'index']);
+Route::get('/employers/{id}', [EmployerController::class, 'show']);
+// Socials
+Route::get('/employers', [EmployerController::class, 'index']);
+Route::get('/employers/{id}', [EmployerController::class, 'show']);
+// Industries
+Route::get('/industries', [IndustryController::class, 'index']);
+Route::get('/industries/{id}', [IndustryController::class, 'show']);
+// Company types
+Route::get('/company-types', [CompanyTypeController::class, 'index']);
+Route::get('/company-types/{id}', [CompanyTypeController::class, 'show']);
 
 /*
 |--------------------------------------------------------------------------
@@ -49,105 +84,73 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::group(['middleware' => ['auth:sanctum']], function () {
     // Auth
     Route::post('/logout', [AuthController::class, 'logout']);
-    // Application statuses
-    Route::get('/application-statuses', [ApplicationStatusController::class, 'index']);
-    Route::get('/application-statuses/{id}', [ApplicationStatusController::class, 'show']);
-    // Employers
-    Route::get('/employers', [EmployerController::class, 'index']);
-    Route::get('/employers/{id}', [EmployerController::class, 'show']);
-    // Employer statuses
-    Route::get('/employer-statuses', [EmployerStatusController::class, 'index']);
-    Route::get('/employer-statuses/{id}', [EmployerStatusController::class, 'show']);
-    // Faculties
-    Route::get('/faculties', [FacultyController::class, 'index']);
-    Route::get('/faculties/{id}', [FacultyController::class, 'show']);
-    // Organizations
-    Route::get('/organizations', [OrganizationController::class, 'index']);
-    Route::get('/organizations/{id}', [OrganizationController::class, 'show']);
-    // Specialities
-    Route::get('/specialities', [SpecialityController::class, 'index']);
-    Route::get('/specialities/{id}', [SpecialityController::class, 'show']);
-    // Stages
-    Route::get('/stages', [StageController::class, 'index']);
-    Route::get('/stages/{id}', [StageController::class, 'show']);
-    // Students
-    Route::get('/students', [StudentController::class, 'index']);
-    Route::get('/students/{id}', [StudentController::class, 'show']);
     // Users
     Route::get('/users', [UserController::class, 'index']);
     Route::get('/users/{id}', [UserController::class, 'show']);
     Route::get('/users/search/{nickname}', [UserController::class, 'search']);
-    Route::put('/users/{id}', [UserController::class, 'update']);
-    // Vacancies
-    Route::get('/vacancies', [VacancyController::class, 'index']);
-    Route::get('/vacancies/{id}', [VacancyController::class, 'show']);
-    // Events
-    Route::get('/events', [EventController::class, 'index']);
-    Route::get('/events/{id}', [EventController::class, 'show']);
-    // News
-    Route::get('/news', [NewsController::class, 'index']);
-    Route::get('/news/{id}', [NewsController::class, 'show']);
+    // Application statuses
+    Route::get('/application-statuses', [ApplicationStatusController::class, 'index']);
+    Route::get('/application-statuses/{id}', [ApplicationStatusController::class, 'show']);
+    // Students
+    Route::get('/students', [StudentController::class, 'index']);
+    Route::get('/students/{id}', [StudentController::class, 'show']);
+    Route::put('/students/{id}', [StudentController::class, 'update']);
+    // Roles
+    Route::get('/roles', [RoleController::class, 'index']);
+    Route::get('/roles/{id}', [RoleController::class, 'show']);
 });
 
 // Admin routes
 Route::group(['middleware' => ['auth:sanctum', 'ability:admin']], function () {
-    // Applications
-    Route::get('/applications', [ApplicationController::class, 'index']);
-    Route::get('/applications/{id}', [ApplicationController::class, 'show']);
-    Route::delete('/applications/{id}', [ApplicationController::class, 'destroy']);
     // Application statuses
     Route::post('/application-statuses', [ApplicationStatusController::class, 'store']);
     Route::put('/application-statuses/{id}', [ApplicationStatusController::class, 'update']);
     Route::delete('/application-statuses/{id}', [ApplicationStatusController::class, 'destroy']);
+    // Audience
+    Route::post('/audience', [AudienceController::class, 'store']);
+    Route::put('/audience/{id}', [AudienceController::class, 'update']);
+    Route::delete('/audience/{id}', [AudienceController::class, 'destroy']);
     // Employers
     Route::post('/employers', [EmployerController::class, 'store']);
     Route::delete('/employers/{id}', [EmployerController::class, 'destroy']);
-    // Employer statuses
-    Route::post('/employer-statuses', [EmployerStatusController::class, 'store']);
-    Route::put('/employer-statuses/{id}', [EmployerStatusController::class, 'update']);
-    Route::delete('/employer-statuses/{id}', [EmployerStatusController::class, 'destroy']);
     // Faculties
     Route::post('/faculties', [FacultyController::class, 'store']);
     Route::put('/faculties/{id}', [FacultyController::class, 'update']);
     Route::delete('/faculties/{id}', [FacultyController::class, 'destroy']);
-    // Organizations
-    Route::post('/organizations', [OrganizationController::class, 'store']);
-    Route::put('/organizations/{id}', [OrganizationController::class, 'update']);
-    Route::delete('/organizations/{id}', [OrganizationController::class, 'destroy']);
-    // Roles
-    Route::get('/roles', [RoleController::class, 'index']);
-    Route::get('/roles/{id}', [RoleController::class, 'show']);
+    // Roles;
     Route::post('/roles', [RoleController::class, 'store']);
     Route::put('/roles/{id}', [RoleController::class, 'update']);
     Route::delete('/roles/{id}', [RoleController::class, 'destroy']);
-    // Specialities
-    Route::post('/specialities', [SpecialityController::class, 'store']);
-    Route::put('/specialities/{id}', [SpecialityController::class, 'update']);
-    Route::delete('/specialities/{id}', [SpecialityController::class, 'destroy']);
-    // Stages
-    Route::post('/stages', [StageController::class, 'store']);
-    Route::put('/stages/{id}', [StageController::class, 'update']);
-    Route::delete('/stages/{id}', [StageController::class, 'destroy']);
     // Students
     Route::post('/students', [StudentController::class, 'store']);
     Route::delete('/students/{id}', [StudentController::class, 'destroy']);
     // Users
     Route::post('/users', [UserController::class, 'store']);
+    Route::put('/users/{id}', [UserController::class, 'update']);
     Route::delete('/users/{id}', [UserController::class, 'destroy']);
-    // Events
-    Route::post('/events', [EventController::class, 'store']);
-    Route::put('/events/{id}', [EventController::class, 'update']);
-    Route::delete('/events/{id}', [EventController::class, 'destroy']);
-    // News
-    Route::post('/news', [NewsController::class, 'store']);
-    Route::put('/news/{id}', [NewsController::class, 'update']);
-    Route::delete('/news/{id}', [NewsController::class, 'destroy']);
+    // Vacancy types
+    Route::post('/vacancy-types', [IndustryController::class, 'store']);
+    Route::put('/vacancy-types/{id}', [IndustryController::class, 'update']);
+    Route::delete('/vacancy-types/{id}', [IndustryController::class, 'destroy']);
+    // Vacancy functions
+    Route::post('/vacancy-functions', [VacancyFunctionController::class, 'store']);
+    Route::put('/vacancy-functions/{id}', [VacancyFunctionController::class, 'update']);
+    Route::delete('/vacancy-functions/{id}', [VacancyFunctionController::class, 'destroy']);
+    // Industries
+    Route::post('/industries', [IndustryController::class, 'store']);
+    Route::put('/industries/{id}', [IndustryController::class, 'update']);
+    Route::delete('/industries/{id}', [IndustryController::class, 'destroy']);
+    // Company types
+    Route::post('/company-types', [CompanyTypeController::class, 'store']);
+    Route::put('/company-types/{id}', [CompanyTypeController::class, 'update']);
+    Route::delete('/company-types/{id}', [CompanyTypeController::class, 'destroy']);
 });
 
 // Employer routes
 Route::group(['middleware' => ['auth:sanctum', 'ability:admin,employer']], function () {
     // Applications
     Route::get('/my-vacancies-applications', [ApplicationController::class, 'indexVacanciesApplications']);
+    Route::get('/applications', [ApplicationController::class, 'index']);
     Route::put('/applications/{id}', [ApplicationController::class, 'update']);
     // Employers
     Route::put('/employers/{id}', [EmployerController::class, 'update']);
@@ -155,13 +158,28 @@ Route::group(['middleware' => ['auth:sanctum', 'ability:admin,employer']], funct
     Route::post('/vacancies', [VacancyController::class, 'store']);
     Route::put('/vacancies/{id}', [VacancyController::class, 'update']);
     Route::delete('/vacancies/{id}', [VacancyController::class, 'destroy']);
+    // News
+    Route::post('/news', [NewsController::class, 'store']);
+    Route::put('/news/{id}', [NewsController::class, 'update']);
+    Route::delete('/news/{id}', [NewsController::class, 'destroy']);
+    // Events
+    Route::post('/events', [EventController::class, 'store']);
+    Route::put('/events/{id}', [EventController::class, 'update']);
+    Route::delete('/events/{id}', [EventController::class, 'destroy']);
+    // Socials
+    Route::post('/socials', [SocialController::class, 'store']);
+    Route::put('/socials/{id}', [SocialController::class, 'update']);
+    Route::delete('/socials/{id}', [SocialController::class, 'destroy']);
 });
 
 // Student routes
 Route::group(['middleware' => ['auth:sanctum', 'ability:admin,student']], function () {
     // Applications
     Route::get('/my-applications', [ApplicationController::class, 'indexStudentApplications']);
+    Route::get('/applications', [ApplicationController::class, 'index']);
     Route::post('/applications', [ApplicationController::class, 'store']);
+    Route::put('/applications/{id}', [ApplicationController::class, 'update']);
+    Route::delete('/applications/{id}', [ApplicationController::class, 'destroy']);
     // Students 
     Route::put('/students/{id}', [StudentController::class, 'update']);
 });
